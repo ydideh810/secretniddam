ae608538d0438afc4b596450dd3a1e3b0d3b1441
ydideh810 <yd.baller810@gmail.com>
2024-06-10 12:24:40 -0400

    Update UserInput.tsx

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -42,7 +42,7 @@ function UserInput({
         href="#"
         onClick={() =>
           alert(
-            "WhimsyWorks, Inc. provides this open source software and website as-is and makes no representations or warranties of any kind concerning its accuracy, safety, or suitability. The user assumes full responsibility for any consequences resulting from its use. WhimsyWorks, Inc. expressly disclaims all liability for any direct, indirect, or consequential harm that may result."
+            "Jiileh Inc. provides this open source software and website as-is and makes no representations or warranties of any kind concerning its accuracy, safety, or suitability. The user assumes full responsibility for any consequences resulting from its use. WhimsyWorks, Inc. expressly disclaims all liability for any direct, indirect, or consequential harm that may result."
           )
         }
         className="text-xs text-gray-400 hover:underline mt-2 text-right flex justify-end w-full"
