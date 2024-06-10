29262f4c4e573c086df100e0b38c448755a3a131
ydideh810 <yd.baller810@gmail.com>
2024-06-10 15:45:12 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -216,7 +216,7 @@ function App() {
                   from the local storage on your computer.
                 </p>
                 <p>Supported browsers: Chrome, Edge (GPU required)</p>
-                <p>Note: Chatbot performance is dependent </p>
+                <p>(Note: Chatbot performance is dependent on GPU performance)</p>
               </h2>
             </div>
           </div>
